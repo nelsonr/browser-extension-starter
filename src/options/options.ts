8acf488eb9074663435614760edc78e008ca5b56
Nelson Rodrigues <nelsonrodrigues87@outlook.com>
2022-11-09 20:29:26 +0000

    Add info on the Options page

--- src/options/options.ts
+++ src/options/options.ts
@@ -1,3 +1,6 @@
+// Options pages are usually reserved for configuration related options of the extension.
+// For more frequent actions, it's better to use the popup page. Like the popup, it's just a regular web page,
+// that can communicate with other parts of the extension by sending messages.
 // Learn more: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/user_interface/Options_pages
 
 const app = document.querySelector("#app");
@@ -5,6 +8,8 @@ const app = document.querySelector("#app");
 if (app) {
     app.innerHTML = `
         <h1>Options Page</h1>
+        
+        <p>Hi there nothing much to see here for now</p>
     `;
 }
 

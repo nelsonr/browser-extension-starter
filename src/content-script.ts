255bb351fdd7b6c6294ab11925baa17988986b7a
Nelson Rodrigues <nelsonrodrigues87@outlook.com>
2022-11-09 20:33:36 +0000

    Update info in Content Script

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,4 +1,4 @@
-// This content script will be loaded in every browser page (this can be configured).
+// This content script will be loaded in every browser page (this can be configured in the manifest file).
 // It's recommended to rename this file to something more meaningful so it's easy to find with the browser devtools.
 // Learn more: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Content_scripts
 
